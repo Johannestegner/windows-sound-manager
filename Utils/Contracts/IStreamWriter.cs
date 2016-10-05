d9f4e53bd219b720dd18699612d4f581e2c54602
simius <simiust@gmail.com>
2016-10-05 16:21:13 +0200

    Interface for StreamWriter wrapper and implementation for same in both util and tests assemblies.

---
+++ Utils/Contracts/IStreamWriter.cs
@@ -0,0 +1,50 @@
+﻿#region License etc.
+
+// ///////////////////////////////////////////////////////////////////////////////////
+// File IStreamWriter.cs is part of the "Windows Sound Manager" project.
+// Created 2016 09 29 by Johannes Tegnér
+// ///////////////////////////////////////////////////////////////////////////////////
+// MIT License
+// Copyright (c) 2016 Johannes Tegnér
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// //////////////////////////////////////////////////////////////////////////////////
+// Source: https://github.com/Johannestegner/windows-sound-manager
+// Issue tracker: https://github.com/Johannestegner/windows-sound-manager/issues
+// Contact: <Johannes Tegnér> jitedev@gmail.com
+// //////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+using System;
+
+namespace JohannesTegner.Utils.Contracts
+{
+  /// <summary>
+  ///   Interface for stream writer class.
+  ///   Should be implemented and passed into internal constructs using stream writers
+  ///   instead of the System.IO.StreamWriter class.
+  ///   This to make testability better.
+  /// </summary>
+  public interface IStreamWriter : IDisposable
+  {
+    /// <summary>
+    ///   Write line to stream.
+    /// </summary>
+    /// <param name="value"></param>
+    void WriteLine(string value);
+  }
+}
